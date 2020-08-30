bdeec9709083acf9f6d5f780d10503a57d54a1d5
Adefemi Oseni <adefemi.oseni@algorismng.com>
2020-08-30 13:43:16 +0100

    updated

--- django_api/settings.py
+++ django_api/settings.py
@@ -65,7 +65,10 @@ CLOUDINARY_STORAGE = {
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20
+    "PAGE_SIZE": 20,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
